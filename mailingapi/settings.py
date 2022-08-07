384b1a17beb1ca1d822b9e9dd497099349402945
gleb-skobinsky <90451272+gleb-skobinsky@users.noreply.github.com>
2022-08-07 15:41:33 +0300

    Update settings.py

--- mailingapi/settings.py
+++ mailingapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5d&crfl3+&o%^%_ru^c=1rlpfgy9wn8=7*k5c)(-ttt6eg6(y3'
+SECRET_KEY = '[YOUR_SECRET_KEY]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
